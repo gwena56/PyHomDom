62bc53808e230f12ef2adc5357e981bba4212d28
gwena56 <gwena56@gmail.com>
2017-07-21 22:28:53 +0200

    UDP
    Installation du code Domoticz

--- serveurUDP.py
+++ serveurUDP.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import socket
-
+import json
+import requests
+import urllib
 UDP_IP = "192.168.1.252"
 UDP_PORT = 8888
+headers = {'content-type':'application/json','Authorization':'Basic a2VyaG91ZW50Om5heGl6aGU0'}
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 while True:
     data, addr = sock.recvfrom(1024)
     if (data == "0100FF") :
-        print "received message:", data
+        url = 'http://127.0.0.1:8084/json.htm?type=command&param=switchlight&idx=2&switchcmd=Toggle'
+        r = requests.get(url)
+        device = r.json()
+        print device
