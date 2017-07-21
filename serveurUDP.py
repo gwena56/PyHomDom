bdf942df7c8701c698a169bd96763fc5f19d6df1
gwena56 <gwena56@gmail.com>
2017-07-21 22:05:45 +0200

    UDP
    Modification du serveur UDP

--- serveurUDP.py
+++ serveurUDP.py
@@ -8,4 +8,5 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 while True:
     data, addr = sock.recvfrom(1024)
-    print "received message:", data
+    if (data == "0100FF") :
+        print "received message:", data
